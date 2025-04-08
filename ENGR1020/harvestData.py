8500cb65bc8a1067a89c0fcc5e95a8764a3f56a6
TheBigBuns27 <82857271+TheBigBuns27@users.noreply.github.com>
2025-04-08 15:09:14 -0400

    Update harvestData.py

--- ENGR1020/harvestData.py
+++ ENGR1020/harvestData.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
 def setDf():
-    dir = path.Path(__file__).abspath()
-    sys.append.path(dir.parent.parent)
-    
-    roomAssignments = pd.read_excel("./MR5 Room Number Assignments.xlsx")
+    roomAssignments = pd.read_excel("MR5 Room Number Assignments.xlsx")
     roomAssignments.set_index("Room Number", inplace=True, drop=True)
     roomAssignments.index.name = None
     return roomAssignments
