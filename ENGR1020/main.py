ebd55fe77c2f695b705d79212e667042ed4390f4
TheBigBuns27 <82857271+TheBigBuns27@users.noreply.github.com>
2025-04-16 11:21:16 -0400

    Uploading work from local.

--- ENGR1020/main.py
+++ ENGR1020/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import harvestData as h
+import backend as b
 #For running: streamlit run main.py [-- script args]
 
 st.title(":blue[Welcome to] :orange[MR5] :blue[Navigation Tool!]")
@@ -8,19 +9,33 @@ st.write("Please enter a professor's name, OR enter a room number and directions
 roomNums = h.roomNums()
 locations = h.locations()
 col1, col2 = st.columns(2)
-accessiblity = st.checkbox("Accessiblity Needed?")
+start = "Checkpoint " + str(st.query_params.get("checkpoint"))
 
 with col1:
     end1 = st.selectbox("Please choose a room number", roomNums)
 with col2:
     end2 = st.selectbox("Please choose a professor or location name", locations)
+accessibility = bool(st.checkbox("Accessibility Needed?"))
 
 if end1 != "BLANK" and end2 != "BLANK":
     st.write("Only select from 1 dropdown.")
+    end = "2 selected"
 elif end1 != "BLANK":
     st.write("Below are directions to get to " + str(end1) + ":")
+    end = str(end1)
+    count = 1
+    for direction in b.navigateMR5(start, end, accessibility):
+        st.write("Step " + str(count) + ". " + direction)
+        count += 1
 elif end2 != "BLANK":
     st.write("Below are directions to get to " + str(end2) + ":")
+    end = h.convertRoomToInt(end2)
+    count = 1
+    for direction in b.navigateMR5(start, end, accessibility):
+        st.write("Step " + str(count) + ". " + direction)
+        count += 1
+else:
+    end = "0"
 
-start = "Checkpoint 1"
-end = st.query_params.get("checkpoint")
+
+
